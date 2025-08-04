226aa504199f9197da901a17ae8f90c7fc7d1111
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2025-08-04 13:57:12 +0530

    Fix 404 handler middleware definition in server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
 });
 
 // 404 handler
-app.use('*', (req, res) => {
+app.use((req, res) => {
   res.status(404).json({ 
     error: 'Route not found',
     path: req.originalUrl 
